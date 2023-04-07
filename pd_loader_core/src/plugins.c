ca59ffd370d9eeaa0d615ca5541d76afe0b8d1bb
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-04-07 13:46:45 -0400

    plugins: Refactoring and cleanup, remove plugin_main.

--- pd_loader_core/src/plugins.c
+++ pd_loader_core/src/plugins.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <malloc.h>
 
+// Reduce the size of Windows.h to improve compile time.
 #define WIN32_LEAN_AND_MEAN
 #define NOCOMM
 #define NOCLIPBOARD
 #define NODRAWTEXT
 #define NOMB
-
 #include <Windows.h>
 
 #include <physfs.h>
@@ -14,32 +15,21 @@
 #include "path.h"
 #include "plugins.h"
 
-static const char loader_msg[] = "[\033[32mPlugin Loader\033[0m]";
-static const char loader_warn[] = "[\033[33mPlugin Loader\033[0m]";
+static const char loader_msg[] = "[\033[32mPlugin Loader\033[0m]";  // Green
+static const char loader_warn[] = "[\033[33mPlugin Loader\033[0m]"; // Yellow
+static const char loader_err[] = "[\033[31mPlugin Loader\033[0m]";  // Red
 
-HMEMORYMODULE* plugin_handles = NULL;
+// We use this to pair the filename with a handle, to implement plugin_get_module_handle().
+typedef struct {
+    char* filename;
+    HMEMORYMODULE handle;
+}module;
+
+static module* loaded_modules = NULL;
 uint32_t plugin_count = 0;
-
-uint32_t plugin_get_count() {
-    return plugin_count;
-}
-
-HMEMORYMODULE* plugin_get_handles() {
-    return plugin_handles;
-}
-
-void plugin_cleanup(void* plugin_handle) {
-    MemoryFreeLibrary(plugin_handle);
-    for (uint32_t i = 0; i < plugin_count; i++) {
-        if (plugin_handles[i] == plugin_handle) {
-            plugin_handles[i] = NULL;
-        }
-    }
-}
 
 // This casting is really annoying, but it will just throw errors about the slightly different types if I don't.
 const plugin_api api = {
-        .plugin_cleanup        = plugin_cleanup,
         .PHYSFS_exists         = (bool (*)(const char*)) PHYSFS_exists,
         .PHYSFS_openRead       = (void*(*)(const char*)) PHYSFS_openRead,
         .PHYSFS_openWrite      = (void*(*)(const char*)) PHYSFS_openWrite,
@@ -52,14 +42,15 @@ const plugin_api api = {
         .PHYSFS_close          = (int (*)(void*)) PHYSFS_close,
         .PHYSFS_enumerateFiles = PHYSFS_enumerateFiles,
         .PHYSFS_freeList       = PHYSFS_freeList,
-        .plugin_get_count      = plugin_get_count,
-        .plugin_get_handles    = plugin_get_handles,
         .plugin_get_proc_address = (void *(*)(void *, const char *)) MemoryGetProcAddress
 };
 
 HMEMORYMODULE custom_load_library(LPCSTR name, void* unused) {
-    char path[MAX_PATH] = "/plugins/";
-    strncat(path, name, MAX_PATH - sizeof("/plugins/"));
+    // MAX_PATH should be plenty unless someone makes an absolutely ridiculous filename.
+    char path[MAX_PATH] = {0};
+    sprintf(path, "/plugins/%s", name);
+
+    // Try to load from the VFS, allows us to load dependencies from plugins.
     if (PHYSFS_exists(path)) {
         PHYSFS_File* module_file = PHYSFS_openRead(path);
         if (module_file == PHYSFS_ERR_OK) {
@@ -72,8 +63,9 @@ HMEMORYMODULE custom_load_library(LPCSTR name, void* unused) {
             }
         }
     }
-    // It doesn't exist in the search path or we failed to load the DLL, fall back to the default.
-    LoadLibraryA(name);
+
+    // It doesn't exist in the search path (or we failed to load the DLL), fall back to the default.
+    return LoadLibraryA(name);
 }
 
 void load_plugins() {
@@ -82,47 +74,46 @@ void load_plugins() {
 
     // Find out how many plugins are in the folder
     for (char**i = file_list; *i != NULL; i++) {
-        if (path_has_extension(*i, ".dll")) {
+        if (path_has_extension(*i, ".dll") || path_has_extension(*i, ".exe")) {
             plugin_count++;
         }
     }
-    plugin_handles = calloc(plugin_count, sizeof(*plugin_handles));
+    // Allocate enough memory to store information for every module, plus some wiggle room just in case.
+    loaded_modules = calloc(plugin_count + 10, sizeof(*loaded_modules));
 
     uint32_t idx = 0;
     for (char** i = file_list; *i != NULL; i++) {
-        if (path_has_extension(*i, ".dll")) {
+        if (path_has_extension(*i, ".dll") || path_has_extension(*i, ".exe")) {
             char full_path[MAX_PATH] = {0};
 
             // Get full virtual filesystem path.
             sprintf(full_path, "%s%s", dir, *i);
 
+            // Read file into a buffer and load it with MemoryModule.
             PHYSFS_File* dll_file = PHYSFS_openRead(full_path);
-            if (dll_file != NULL) {
+            if (dll_file == NULL) {
+                printf("%s: Failed to open %s for reading (PHYSFS).\n", loader_err, full_path);
+            }
+            else
+            {
                 int64_t filesize = PHYSFS_fileLength(dll_file);
                 uint8_t* plugin_data = calloc(1, filesize);
-                if (plugin_data != NULL) {
+                if (plugin_data == NULL) {
+                    printf("%s: Failed to allocate %lli bytes for %s (PHYSFS).\n", loader_err, filesize, full_path);
+                }
+                else {
                     PHYSFS_readBytes(dll_file, plugin_data, filesize);
                     // This is a really long one-liner, but this is just MemoryLoadLibrary() but using custom_load_library() to load dependencies.
-                    plugin_handles[idx] = MemoryLoadLibraryEx(plugin_data, filesize, MemoryDefaultAlloc, MemoryDefaultFree, custom_load_library, MemoryDefaultGetProcAddress, MemoryDefaultFreeLibrary, NULL);
+                    loaded_modules[idx].handle = MemoryLoadLibraryEx(plugin_data, filesize, MemoryDefaultAlloc, MemoryDefaultFree, custom_load_library, MemoryDefaultGetProcAddress, MemoryDefaultFreeLibrary, NULL);
 
-                    PLUGINMAIN plugin_main = (PLUGINMAIN) MemoryGetProcAddress(plugin_handles[idx], "plugin_main");
-                    FARPROC dll_main = MemoryGetProcAddress(plugin_handles[idx], "DllMain");
-                    if (dll_main != NULL) {
-                        if (plugin_main != NULL) {
-                            printf("%s: %s exports DllMain() and plugin_main(), both will be executed.\n", loader_warn, *i);
-                        }
-                        else {
-                            // TODO: This is kind of a long message, maybe shorten it?
-                            printf("%s: %s doesn't export plugin_main(). Using plugin_main() is preferred, because it gives access to the virtual filesystem and allows you to unload your plugin.\n", loader_warn, *i);
-                        }
+                    // Copy the filename into the structure.
+                    if (loaded_modules[idx].handle != NULL) {
+                        strcpy(loaded_modules[idx].filename, *i);
                     }
-                    if (plugin_main != NULL) {
-                        plugin_main(plugin_handles[idx], &api);
-                    }
+                    // MemoryLoadLibraryEx automatically calls the DLL/EXE entry point.
+                    printf("%s: Loaded %s.\n", loader_msg, *i);
                 }
             }
-
-            printf("%s: Loaded %s.\n", loader_msg, *i);
             idx++;
         }
     }
