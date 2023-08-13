46f4272489ef5340ce4cb5960963ec35bf8ed108
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-08-13 01:30:20 -0400

    Real mutexes for fake files
    This still doesn't fully work, but it's a lot less unstable than last time. (It just crashes or hangs instead of partially loading files or loading garbage data)

--- pd_loader_core/src/hooks.c
+++ pd_loader_core/src/hooks.c
@@ -21,7 +21,18 @@
 typedef struct {
     PHYSFS_File* physfs_handle;
     HANDLE win_handle;
+    bool is_physfs_archive;
 }file_handle;
+
+// Stores pairs of PhysicsFS and Windows file handles.
+// Enough for the game to open 32,768 files at once. There are roughly 3,800 files in the game.
+file_handle handle_list[0x8000] = {0};
+uint16_t handle_list_pos = 0;
+// Stores which handle pairs are free to be overwritten (by index). When a handle is closed, it's added to this list and the handle pair is set to NULL.
+// When a free handle pair is overwritten, its entry in this list is set to 0xFFFF. The list should be initialized to 0xFFFF.
+uint16_t* free_list = NULL;
+uint16_t free_count = 0;
+
 
 typedef HANDLE (*CREATE_FILE_2)(LPCWSTR, DWORD, DWORD, DWORD, LPCREATEFILE2_EXTENDED_PARAMETERS);
 CREATE_FILE_2 original_CreateFile2 = NULL;
@@ -31,17 +42,11 @@ typedef int (*READ_FILE)(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED);
 READ_FILE addr_ReadFile = NULL;
 READ_FILE original_ReadFile = NULL;
 
+HANDLE read_mutex = NULL;
+
 bool lock_filesystem = true;
 
 HANDLE hook_CreateFile2(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, DWORD dwCreationDisposition, LPCREATEFILE2_EXTENDED_PARAMETERS pCreateExParams) {
-    // During startup, it seems that this function is sometimes called asynchronously. This value is shared across
-    // each of these running instances, so while the function is running it will force other instances of the call to wait.
-    static bool lock_async_calls = false;
-    while (lock_async_calls) {
-        Sleep(1);
-    }
-    lock_async_calls = true;
-
     char path[MAX_PATH] = {0};
     wchar_t wide_path[MAX_PATH] = {0};
     PHYSFS_utf8FromUtf16(lpFileName, path, MAX_PATH);
@@ -49,6 +54,7 @@ HANDLE hook_CreateFile2(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShare
 
     static file_handle handles = {0};
 
+    // Try to open the file with the default function.
     if (PHYSFS_exists(path)) {
         // Get the real path of the file and place it in a wide string.
         swprintf(wide_path, MAX_PATH, L"%hs/%hs", PHYSFS_getRealDir(path), path);
@@ -76,11 +82,13 @@ HANDLE hook_CreateFile2(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShare
         default:
             printf("[UNKNOWN]:");
     }
-    printf(" Opened %s\n", path);
+    // printf(" Opened %s\n", path);
+    printf(" Opened %s [from real path %s]\n", path, PHYSFS_getRealDir(path));
 
-    if (PHYSFS_exists(path) && handles.win_handle == INVALID_HANDLE_VALUE) {
-        printf("Creating a fake file for Windows handle.\n");
-        system("pause");
+    // File exists in PhysicsFS but not in the real filesystem. So we create a blank file of the same size for the
+    // Windows handle.
+    if (handles.physfs_handle != NULL && handles.win_handle == INVALID_HANDLE_VALUE) {
+
         static char fake_path[MAX_PATH] = {0};
         get_ms_esper_path(fake_path);
         {
@@ -96,13 +104,31 @@ HANDLE hook_CreateFile2(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShare
         fclose(fake_file);
 
         PHYSFS_utf8ToUtf16(fake_path, wide_path, MAX_PATH);
+        printf("Opening a fake file for Windows at %s.\n", fake_path);
         handles.win_handle = original_CreateFile2(wide_path, dwDesiredAccess, dwShareMode, dwCreationDisposition, pCreateExParams);
         printf("PhysicsFS handle was %p.\n", handles.physfs_handle);
+
+        const char* real_path = PHYSFS_getRealDir(path);
+        if (path_has_extension(real_path, ".zip") || path_has_extension(real_path, ".7z") || path_has_extension(real_path, ".rar")) {
+            handles.is_physfs_archive = true;
+        }
     }
 
     MH_EnableHook(addr_CreateFile2);
 
-    lock_async_calls = false; // Allow waiting call to run.
+    if (free_count == 0) {
+        // Insert new entry in handle list and increment position
+        handle_list[handle_list_pos++] = handles;
+    }
+    else {
+        uint16_t index = free_list[--free_count];
+        free_list[free_count] = 0xFFFF;
+        handle_list[index] = handles;
+    }
+
+    if (handles.is_physfs_archive) {
+        printf("CreateFile2(): Leaving the function.\n");
+    }
     return handles.win_handle;
 }
 
@@ -117,16 +143,43 @@ HANDLE hook_CreateFile2_stall(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD d
 
 // Unused for now.
 int hook_ReadFile(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped) {
-    static bool lock_async_calls = false;
-    while (lock_async_calls) {
-        Sleep(1);
+    uint32_t wait_result = WaitForSingleObject(read_mutex, INFINITE);
+
+    if (wait_result == WAIT_OBJECT_0) {
+        // We got ownership of the mutex
+        file_handle handles = {0};
+        for (uint32_t i = 0; i < handle_list_pos; i++) {
+            if (hFile == handle_list[i].win_handle) {
+                handles = handle_list[i];
+            }
+        }
+
+        int result = 0;
+        if (handles.physfs_handle != NULL && handles.is_physfs_archive) {
+            if (lpOverlapped != NULL) {
+                PHYSFS_seek(handles.physfs_handle, (uint64_t) lpOverlapped->Offset | (uint64_t) lpOverlapped->OffsetHigh);
+            }
+            printf("Using PHYSFS handle.\n");
+            result = PHYSFS_readBytes(handles.physfs_handle, lpBuffer, nNumberOfBytesToRead);
+            char* buf = malloc(nNumberOfBytesToRead);
+            if (buf != NULL) {
+                result = original_ReadFile(hFile, buf, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
+                free(buf);
+            }
+        }
+        else {
+            result = original_ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
+        }
+
+        if (!ReleaseMutex(read_mutex)) {
+            printf("ReadFile(): Failed to release mutex!\n");
+        }
+        return result;
     }
-    lock_async_calls = true;
-
-    int result = original_ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
-
-    lock_async_calls = false; // Allow waiting call to run.
-    return result;
+    else if (wait_result == WAIT_ABANDONED) {
+        printf("ReadFile(): Read mutex abandoned. Panic time!\n");
+        return 0;
+    }
 }
 
 void hooks_unlock_filesystem() {
@@ -167,6 +220,7 @@ bool hooks_setup_lock_files() {
         printf("Failed to create hook.\n");
         return false;
     }
+    read_mutex = CreateMutex(NULL, FALSE, NULL);
 
     if (MH_EnableHook(addr_CreateFile2) != MH_OK) {
         printf("Failed to enable hook.\n");
